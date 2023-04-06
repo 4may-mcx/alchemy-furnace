796806d73a8dbba1dc9598c6b3f9a6d80d21f645
muchaoxu <1522928438@qq.com>
2023-04-06 22:56:09 +0800

    add: style fix

--- src/content/components/check-list/index.js
+++ src/content/components/check-list/index.js
@@ -1,25 +1,11 @@
 import { List } from "antd";
 
-const data = [
-  "Racing",
-  "Japanese",
-  "Australian",
-  "Man",
-  "Los Angeles",
-  "Los Angeles",
-  "Los Angeles",
-  "Los Angeles",
-  "Los Angeles",
-  "Los Angeles",
-  "Los Angeles",
-];
+const data = ["Racing", "Japanese", "Australian", "Man"];
 
 const CheckList = () => (
   <>
     <List
       size="small"
-      header={<div>Header</div>}
-      footer={<div>Footer</div>}
       bordered
       dataSource={data}
       renderItem={(item) => <List.Item>{item}</List.Item>}
