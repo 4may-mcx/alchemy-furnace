bae39b48ca3efc1a8491864c9f765d2813e644a4
xumuchao <xumuchao@kanyun.com>
2023-04-06 14:30:58 +0800

    init

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+import "./index.module.scss";
+
 const Popup = () => {
-  return <div>hello popup</div>;
+  return (
+    <div>
+      <h1>hello popup</h1>
+      <Button>click</Button>
+    </div>
+  );
 };
 
 export default Popup;
